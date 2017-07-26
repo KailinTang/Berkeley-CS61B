4fb207714941c48c765fcbca9f351fc51ebc6e04
KailinTang <kailintang1995@gmail.com>
2017-07-26 11:16:12 +0800

    lab added

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Kailin Tang on 2017/7/26.
+ */
+public class HelloNumbers {
+    public static void main(String[] args){
+        int sum = 0;
+        for(int i = 0; i < 10; i++){
+            sum += i;
+            System.out.print(sum + " ");
+        }
+    }
+}
