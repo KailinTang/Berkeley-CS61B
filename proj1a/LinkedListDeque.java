6a70955383f983d558d38bc0559ef25ed5e06e56
KailinTang <kailintang1995@gmail.com>
2017-07-30 14:15:53 +0800

    proj1a 7.30a

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,11 +1,11 @@
-public class LinkedListDeque{
+public class LinkedListDeque<Item> {
 
     private class IntNode {
         public IntNode prev;
-        public int item;
+        public Item item;
         public IntNode next;
 
-        public IntNode (IntNode prev, int item, IntNode next) {
+        public IntNode (IntNode prev, Item item, IntNode next) {
             this.prev = prev;
             this.item = item;
             this.next = next;
@@ -15,28 +15,28 @@ public class LinkedListDeque{
     private IntNode sentinel;
     private int size;
 
-    public SLList(int x) {
-        sentinel = new IntNode(null, 0, null);
+    public SLList(Item x) {
+        sentinel = new IntNode(null, null, null);
         sentinel.prev = sentinel;
         sentinel.next = sentinel;
         size = 0;
     }
 
-    public LinkedListDeque(int x) {
+    public LinkedListDeque(Item x) {
         sentinel = new IntNode(null, 0, null);
         sentinel.next = new IntNode(sentinel, x, sentinel);
         sentinel.prev = sentinel.next;
         size = 1;
     }
 
-    public void addFirst(int x) {
+    public void addFirst(Item x) {
         IntNode newNode = new IntNode(sentinel, x, sentinel.next);
         sentinel.next.prev = newNode;
         sentinel.next = newNode;
         size += 1;
     }
 
-    public void addLast(int x) {
+    public void addLast(Item x) {
         IntNode newNode = new IntNode(sentinel.prev, x, sentinel);
         sentinel.prev.next = newNode;
         sentinel.prev = newNode;
@@ -56,5 +56,15 @@ public class LinkedListDeque{
     public int size() {
         return this.size;
     }
+
+    public void printDeque() {
+        IntNode tempNode = sentinel.next;
+        while(!sentinel.equals(tempNode)){
+            System.out.println(tempNode.item);
+            tempNode = tempNode.next;
+        }
+    }
+
+    public Item removeFirst
 
 }
