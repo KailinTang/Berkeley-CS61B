9112d7ae25e6361709843fee083c4572d6ea2c85
KailinTang <kailintang1995@gmail.com>
2017-08-02 00:10:05 +0800

    AList Version of Deque half way done

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -88,11 +88,43 @@ public class ArrayDeque<Item> {
     }
 
     public Item removeFirst() {
-        return null;
+        if (this.nextFirst == this.size-1) {
+            Item tempItem = items[0];
+            this.items[0] = null;
+            this.nextFirst = 0;
+            this.size -= 1;
+            return tempItem;
+        }
+        else if(!this.isEmpty()) {
+            Item tempItem = items[this.nextFirst + 1];
+            this.items[this.nextFirst + 1] = null;
+            this.nextFirst += 1;
+            this.size -= 1;
+            return tempItem;
+        }
+        else {
+            return null;
+        }
     }
 
     public Item removeLast() {
-        return null;
+        if(this.nextLast == 0) {
+            Item tempItem = items[this.size-1];
+            this.items[this.size-1] = null;
+            this.nextLast = this.size-1;
+            this.size -= 1;
+            return tempItem;
+        }
+        else if(!this.isEmpty()) {
+            Item tempItem = items[this.nextLast - 1];
+            this.items[this.nextLast -1] = null;
+            this.nextLast -= 1;
+            this.size -= 1;
+            return tempItem;
+        }
+        else{
+            return null;
+        }
     }
 
     public Item get(int index) {
@@ -102,15 +134,19 @@ public class ArrayDeque<Item> {
     public static void main(String[] args) {
         ArrayDeque<String> arrayDeque = new ArrayDeque<>();
         arrayDeque.size();
-        arrayDeque.addFirst("Hello");
-        arrayDeque.addFirst("Hello");
-        arrayDeque.addFirst("Hello");
-        arrayDeque.addFirst("Hello");
-        arrayDeque.addLast("Hello");
-        arrayDeque.addLast("Hello");
-        arrayDeque.addLast("Hello");
-        arrayDeque.addLast("Hello");
-        arrayDeque.addFirst("Hello");
+        arrayDeque.addFirst("1");
+        arrayDeque.addFirst("2");
+        arrayDeque.addFirst("3");
+        arrayDeque.addFirst("4");
+        arrayDeque.addFirst("5");
+        arrayDeque.addLast("6");
+        arrayDeque.addLast("7");
+        arrayDeque.addFirst("8");
+        arrayDeque.removeLast();
+        arrayDeque.removeLast();
+        arrayDeque.removeLast();
+        arrayDeque.removeLast();
+        arrayDeque.removeLast();
     }
 
 }
